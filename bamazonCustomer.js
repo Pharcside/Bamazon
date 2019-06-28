0761edfbefb0592fdd4c90f07a1fecca1b2e2a6a
Brian Corbett <briancorbett@brighthouse.com>
2019-06-28 17:31:47 -0400

    function to list inventory created

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,17 +1,19 @@
+// variables to require installed npm packages
 var mysql = require("mysql");
 var inquirer = require('inquirer');
+var Table = require('cli-table-redemption');
 
-
+// Connection to the database
 var connection = mysql.createConnection({
   host: "localhost",
 
-  // Your port; if not 3306
+  
   port: 3307,
 
-  // Your username
+  
   user: "root",
 
-  // Your password
+  
   password: "root",
   database: "bamazon"
 });
@@ -19,5 +21,29 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
-  connection.end();
+ inventory();
 });
+// 
+function inventory(){
+    var table = new Table({
+        head: ["ID", "Item", "Department", "Price", "Stock"],
+        colWidths: [10, 50, 30, 25, 25]
+});
+
+inventory();
+
+function listInventory(){
+    for (var i = 0; i < res.length; i++) {
+        var itemID = res[i].itemID;
+        var productName = res[i].product_name;
+        var departmentName = res[i].department_name;
+        var price = res[i].price;
+        var stockQuantity = res[i].stock_quantity;
+
+    table.push [itemID, productName, departmentName, price, stockQuantity ]    
+
+    }
+}
+
+
+}

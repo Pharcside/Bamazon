1fe51cf5a5ecb943ac23b9c35fdc8b54bb449889
Brian Corbett <briancorbett@brighthouse.com>
2019-06-30 16:53:16 -0400

    Started work on inventory function, continuePrompt function and selectipon prompt function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,45 +5,122 @@ var Table = require('cli-table-redemption');
 
 // Connection to the database
 var connection = mysql.createConnection({
-  host: "localhost",
+    host: "localhost",
 
-  
-  port: 3307,
 
-  
-  user: "root",
+    port: 3307,
 
-  
-  password: "root",
-  database: "bamazon"
+
+    user: "root",
+
+
+    password: "root",
+    database: "bamazon"
 });
 
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId + "\n");
- inventory();
+connection.connect(function (err, ) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    connection.end()
+    startPromt();
 });
-// 
-function inventory(){
+
+function startPrompt() {
+    inquirer.prompt([{
+        type: "confirm",
+        name: "confirm",
+        message: "Would you like to view the inventory?",
+        default: true
+
+    }]).then(function (user) {
+        if (user.confirm === true) {
+            inventory();
+        } else {
+            console.log("Come back again soon!")
+        }
+    })
+};
+
+function inventory() {
     var table = new Table({
         head: ["ID", "Item", "Department", "Price", "Stock"],
         colWidths: [10, 50, 30, 25, 25]
-});
+    });
 
-inventory();
+    listInventory();
 
-function listInventory(){
-    for (var i = 0; i < res.length; i++) {
-        var itemID = res[i].itemID;
-        var productName = res[i].product_name;
-        var departmentName = res[i].department_name;
-        var price = res[i].price;
-        var stockQuantity = res[i].stock_quantity;
+    function listInventory() {
+        connection.query("SELECT * FROM products", function (err, res) {
+            for (var i = 0; i < res.length; i++) {
+                var itemID = res[i].itemID;
+                var productName = res[i].product_name;
+                var departmentName = res[i].department_name;
+                var price = res[i].price;
+                var stockQuantity = res[i].stock_quantity;
 
-    table.push [itemID, productName, departmentName, price, stockQuantity ]    
+                table.push[itemID, productName, departmentName, price, stockQuantity]
 
+            };
+            console.log("~~~~~~~~~~~~~~~~Current Inventory~~~~~~~~~~~~~~~~~~~~~~~~");
+            console.log(table.toString());
+            connection.end();
+            continuePrompt();
+
+
+        }
+
+
+        )
     }
 }
 
 
+continuePrompt() {
+    inquirer.prompt([{
+        type: "confirm",
+        name: "confirm",
+        message: "Would you like to purchace an item?",
+        default: true
+
+    }]).then(function (user) {
+        if (user.continue === true) {
+            selectionPrompt();
+        } else {
+            console.log("Come back soon!")
+        }
+    });
 }
+
+function selectionPrompt() {
+
+    inquirer.prompt([{
+
+        type: "input",
+        name: "inputId",
+        message: "Enter ID numer of the item you would like to purchase.",
+    },
+    {
+        type: "input",
+        name: "inputNumber",
+        message: "How many units of this item would you like to buy?",
+    }    
+
+    }
+]).then(function (userPurchace) {
+        connection.query("SELECT * FROM products WHERE item_id=?", userPurchace.inputId, function (err, res) {
+            for (var i = 0; i < res.length; i++) {
+                if (userPurchace.inputNumber > res[i]
+                    .stock_quantity) {
+                    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                    console.log("Sorry not enough in stock");
+                    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                    startPrompt();
+
+                } else {
+                    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                    console.log("Great, we can fill your order!")
+                }
+            }
+        }
+
+    }
